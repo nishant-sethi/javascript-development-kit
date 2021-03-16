52f422047077fa420fdd44be3890572c3d7d04ed
nishantsethi <nsethi610@gmail.com>
2021-03-16 22:37:32 +0000

    configured mocha for unit testing

---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+import jsdom from "jsdom";
+import fs from "fs";
+
+describe("first test", () => {
+  it("should pass", () => {
+    expect(2 + 2).to.equal(4);
+  });
+});
+describe("index.html", () => {
+  it("should say hello", () => {
+    const index = fs.readFileSync("./src/index.html", "utf-8");
+    const { JSDOM } = jsdom;
+    const dom = new JSDOM(index);
+    const h1 = dom.window.document.getElementsByTagName("h1")[0];
+    expect(h1.innerHTML).to.equal("hello Worldx");
+    dom.window.close();
+  });
+});
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,9 +11,16 @@ export default {
   },
   plugins: [],
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] },
-      { test: /\.css$/, loaders: ["style", "css"] },
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
