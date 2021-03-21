521fa051e901f13faf045ff38beaeecef5d30eeb
nishantsethi <nsethi610@gmail.com>
2021-03-21 22:32:14 +0000

    configuring production build

--- scripts/distServer.js
+++ scripts/distServer.js
@@ -12,13 +12,6 @@ app.use(express.static("dist"));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../dist/index.html"))
 );
-app.get("/users", (req, res) => {
-  res.json([
-    { id: 1, firstName: "xyz", lastName: "xyz", email: "xyz" },
-    { id: 2, firstName: "xyz", lastName: "xyz", email: "xyz" },
-    { id: 3, firstName: "xyz", lastName: "xyz", email: "xyz" },
-  ]);
-});
 app.listen(port, (err) => {
   if (err) {
     // eslint-disable-next-line no-console
--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
   return getQueryStringParameterByName("useMockApi")
     ? "http://localhost:3001/"
-    : "/";
+    : "https://lit-oasis-73014.herokuapp.com/";
 }
 
 const getQueryStringParameterByName = (name, url) => {
